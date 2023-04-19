9986d4b71e95619f66cc804ae8eddd3e9b00e3ae
deivezera <dzabroski@gmail.com>
2023-04-18 21:56:02 -0300

    passed or not conditional

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -10,23 +10,32 @@ class Program
 
         if (grade >= 90)
         {
-            Console.WriteLine(" Grade: " + "A");
+            Console.WriteLine("Your grade is: " + "A");
         } 
         else if(grade >= 80)
         {
-            Console.WriteLine(" Grade: " + "B");
+            Console.WriteLine("Your grade is: " + "B");
         } 
         else if (grade >= 70)
         {
-            Console.WriteLine(" Grade: " + "C");
+            Console.WriteLine("Your grade is: " + "C");
         } 
         else if (grade >= 60)
         {
-            Console.WriteLine(" Grade: " + "D");
+            Console.WriteLine("Your grade is: " + "D");
         }
         else
         {
-            Console.WriteLine(" Grade: " + "F");
+            Console.WriteLine("Your grade is: " + "F");
+        }
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
         }
     }
 }
