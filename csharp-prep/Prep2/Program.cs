3cf2abd8d4ab03e6c32f4ec809e3532d54a0098d
deivezera <dzabroski@gmail.com>
2023-04-18 21:54:25 -0300

    prep 1 and prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What's your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine(" Grade: " + "A");
+        } 
+        else if(grade >= 80)
+        {
+            Console.WriteLine(" Grade: " + "B");
+        } 
+        else if (grade >= 70)
+        {
+            Console.WriteLine(" Grade: " + "C");
+        } 
+        else if (grade >= 60)
+        {
+            Console.WriteLine(" Grade: " + "D");
+        }
+        else
+        {
+            Console.WriteLine(" Grade: " + "F");
+        }
     }
 }
